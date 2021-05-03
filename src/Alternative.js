fc773e8a58998caaa4d8749d3798c3ea6d874cc2
Ankur Sharma <sharma18ankur@gmail.com>
2021-05-03 10:54:39 +0530

    Added Alternative to index

--- src/Alternative.js
+++ src/Alternative.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
-function App() {
+function Alternative() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
 
@@ -93,4 +93,4 @@ function App() {
   )
 }
 
-export default App
+export default Alternative
